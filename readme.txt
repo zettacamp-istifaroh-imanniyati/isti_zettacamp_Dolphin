267a3657b8f26b3b807624724c6383dd1c1508a8
zettacamp-istifaroh-imanniyati <140967072+zettacamp-istifaroh-imanniyati@users.noreply.github.com>
2023-08-27 14:25:31 +0700

    Create file & directory

---
+++ readme.txt
@@ -0,0 +1,9 @@
+TASK
+Declare two variable with name of your favourite books
+Update value of first with name your least favourite book
+Define second variable as constant and try update the value
+Concat value from variables task number 1 into new variable
+
+Note:
+Try declare more variable using other data type with dynamic value and primitive value
+
---
+++ variable.js
@@ -0,0 +1,38 @@
+
+// Declare 2 variables
+let myFavBook1= "Filosofi Teras";
+const myFavBook2 = "Automic Habbits";
+
+
+
+// Reassign value of variables
+myFavBook1 = 'Harry Potter';
+// myFavBook2 = "The Ordinary People";
+
+console.log(myFavBook1);
+console.log(myFavBook2);
+
+// Concat value from variables task number 1 into new variable
+let favBook = `${myFavBook1} ${myFavBook2}`;
+let favBook2 = myFavBook2.concat(" ", myFavBook1);
+console.log(favBook);
+console.log(favBook2);
+
+
+//Reassign value of variable with different type
+let numb = 20;
+console.log(numb);
+
+numb = 'duapuluh lima';
+console.log(numb);
+
+let isFemale = true;
+isFemale = "Benar";
+console.log(isFemale);
+
+
+let student = "Sasa";
+console.log(student);
+student = {name: "Sasa", age: 20};
+console.log(student);
+
